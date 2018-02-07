3b2b604a7f4048bcc7db249557abfa62f5b99f2d
daniel <daniel@insourceasia.org>
2018-02-07 13:59:27 +0700

    first test

--- SignalRServer/SignalRServer/ChatHub.cs
+++ SignalRServer/SignalRServer/ChatHub.cs
@@ -18,7 +18,7 @@ namespace SignalRServer
             return r * 1000;
         }
         
-        public async Task SendSingle(string name, string message)
+        private async Task SendSingle(string name, string message)
         {
             var msg = message + "|" + DateTime.Now.ToString("HH:mm:ss.ms");
             await Task.Run(() => SendSingleArray(name, msg));
@@ -39,56 +39,8 @@ namespace SignalRServer
             {
                 var msg = message + " - " + i + "|" + DateTime.Now.ToString("HH:mm:ss.ms");
                 tasks.Add(Task.Run(() => SendSingleArray(name, msg)));
-            //    SendSingleArray(name, message + " - " + i);
             }
             Task.WaitAll(tasks.ToArray());
         }
-        
-        //public void SendToAll(string name, string message)
-        //{
-        //    List<Task> tasks = new List<Task>();
-        //    for(int i=0; i<100;i++)
-        //    {
-        //        var msg = "Async Task " + i.ToString();
-        //        tasks.Add(Task.Run(() => asyncFunc(name, msg)));
-        //        //tasks.Add(asyncFunc(name, msg));
-
-        //        //var msg1 = "Sync Task " + i.ToString();
-        //        //tasks.Add(Task.Run(() => syncFunc(name, msg1)));
-        //        //tasks.Add(syncFunc(name, msg1));
-        //    }
-
-        //    Task.WaitAll(tasks.ToArray());
-
-        //}
-
-        //private void syncFunc(string name, string message)
-        //{
-        //    message += ": " + DateTime.Now.ToString();
-        //    //int milliseconds = 5000;
-        //    //await Task.Delay(milliseconds);
-        //    var r = new Random().Next(1, 5);
-        //    //int milliseconds = r*1000;
-        //    //Thread.Sleep(milliseconds);
-        //    Task.Delay(5000).Wait();
-        //    //await Task.Delay(milliseconds);
-        //    message += " //: " + DateTime.Now.ToString() + " **************************************************";
-        //    Trace.WriteLine(message);
-        //    Clients.All.InvokeAsync("sendToAll", name, message);
-        //}
-
-        //private async Task asyncFunc(string name, string message)
-        //{
-        //    message += ": " + DateTime.Now.ToString();
-        //    int milliseconds = 5000;
-        //    //await Task.Delay(milliseconds);
-        //    var r = new Random().Next(1, 5);
-        //    //int milliseconds = r*1000;
-        //    //Thread.Sleep(milliseconds);
-        //    await Task.Delay(milliseconds);
-        //    message += " // " + DateTime.Now.ToString();
-        //    Trace.WriteLine(message);
-        //    await Clients.All.InvokeAsync("sendToAll", name, message);
-        //}
     }
 }
